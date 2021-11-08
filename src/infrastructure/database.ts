51e7add07614640fa550025a8302c16bcdf94a0e
DEV-YukiWatanabe <yu_watanabe@codmon.co.jp>
2021-11-09 08:59:44 +0900

    feat: docker-compose.ymlにhealth_checkを追加

--- src/infrastructure/database.ts
+++ src/infrastructure/database.ts
@@ -3,7 +3,6 @@ import mysql from "mysql2";
 const port = 3333
 export const con = mysql.createConnection({
     host: 'host.docker.internal',
-    // host: "mysql",
     user: 'mysql',
     password: 'mysql',
     port: port
